3f1614bb29d85523a8fa2eff706fdae7a5ae9935
Emmanuelle <emmanuelle.mercadal@medelse.com>
2021-12-08 12:32:21 +0100

    [MF-985] Aria - Get advance

--- src/Resource/Advance.php
+++ src/Resource/Advance.php
@@ -16,7 +16,11 @@ class Advance extends Resource
     public const STATUS_LATE       = 'LATE';
 
     public const CREATE_ADVANCE_URL = '/user/{userId}/advance';
+    public const GET_ADVANCE_URL    = '/user/{userId}/advance/{advanceId}';
 
+    /**
+     * @return array The created advance
+     */
     public function createAdvance(array $data, string $ariaId): array
     {
         $resolver = new CreateAdvanceResolver();
@@ -28,5 +32,18 @@ class Advance extends Resource
         );
 
         return $this->sendRequestFormData(Request::METHOD_POST, $path, $data);
+    }
+
+    /**
+     * @return array The requested advance
+     */
+    public function getAdvance(string $ariaId, string $advanceId): array
+    {
+        $path = str_replace(
+            ['{userId}', '{advanceId}'],
+            [$ariaId, $advanceId],
+            self::GET_ADVANCE_URL
+        );
+        return $this->sendGetRequest($path);
     }
 }
