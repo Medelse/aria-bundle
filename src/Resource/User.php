9901e12f64e190fad369f76ef27d77d5b4d9ce53
Emmanuelle <emmanuelle.mercadal@medelse.com>
2022-02-10 17:37:33 +0100

    Update contract status constants

--- src/Resource/User.php
+++ src/Resource/User.php
@@ -14,10 +14,14 @@ class User extends Resource
     public const KYC_VALID_KEY = 'VALID';
     public const KYC_INVALID_KEY = 'INVALID';
 
-    public const CONTRACT_STATUS_NOT_SEND_KEY = 'notSent';
-    public const CONTRACT_STATUS_PENDING_KEY = 'pending';
-    public const CONTRACT_STATUS_SIGNED_KEY = 'signed';
-    public const CONTRACT_STATUS_COMPLETED_KEY = 'completed';
+    public const USER_CONTRACT_STATUS_NOT_SEND_KEY = 'notSent';
+    public const USER_CONTRACT_STATUS_PENDING_KEY = 'pending';
+    public const USER_CONTRACT_STATUS_SIGNED_KEY = 'signed';
+    public const USER_CONTRACT_STATUS_COMPLETED_KEY = 'completed';
+    public const CONTRACT_STATUS_NOT_SEND_KEY = 'NOTSENT';
+    public const CONTRACT_STATUS_PENDING_KEY = 'PENDING';
+    public const CONTRACT_STATUS_SIGNED_KEY = 'SIGNED';
+    public const CONTRACT_STATUS_COMPLETED_KEY = 'COMPLETED';
 
     public const CREATE_USER_URL = '/user';
     public const UPDATE_USER_URL = '/user/{userId}';
@@ -62,7 +66,7 @@ class User extends Resource
     public function getUser(string $userId): array
     {
         $path = str_replace('{userId}', $userId, self::GET_USER_URL);
-        
+
         return $this->sendGetRequest($path);
     }
 
